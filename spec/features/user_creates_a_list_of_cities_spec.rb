01b07a034a42738497345fd0caeae76992b6dd68
Daniel Castro <castrodd@gmail.com>
2017-08-06 19:40:32 -0400

    Added test for adding false cities

--- spec/features/user_creates_a_list_of_cities_spec.rb
+++ spec/features/user_creates_a_list_of_cities_spec.rb
@@ -23,6 +23,26 @@ RSpec.feature "User creates a list", type: :feature do
     expect(page).to have_content 'New York'
   end
 
+  scenario "User cannot add imaginary cities" do
+    mailbox = Faker::Internet.email
+    key = Faker::Seinfeld.quote
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    fill_in 'Password confirmation', with: key
+    click_button 'Create User'
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    click_button 'Log in'
+    click_link 'Add City'
+    fill_in 'Name', with: 'Ozymandias'
+    click_button 'Add'
+    expect(page).to have_content 'City unavailable.'
+  end
+
   scenario "User can delete cities from list of favorites" do
     mailbox = Faker::Internet.email
     key = Faker::Seinfeld.quote
