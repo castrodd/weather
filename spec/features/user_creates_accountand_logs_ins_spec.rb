251a60ca515e13cc8829b2230d7fe9bc5afbe384
Daniel Castro <castrodd@gmail.com>
2017-08-06 18:46:02 -0400

    More account tests

--- spec/features/user_creates_accountand_logs_ins_spec.rb
+++ spec/features/user_creates_accountand_logs_ins_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'faker'
 
-RSpec.feature "User creates account and logs in", type: :feature do
+RSpec.feature "User creates account", type: :feature do
 
   scenario "User able to sign up" do
     mailbox = Faker::Internet.email
@@ -59,5 +59,22 @@ RSpec.feature "User creates account and logs in", type: :feature do
     fill_in 'Password', with: "Wrong"
     click_button 'Log in'
     expect(page).to have_content "Something was wrong with your email and/or password."
+  end
+
+  scenario "User signs in and logs out" do
+    mailbox = Faker::Internet.email
+    key = Faker::Seinfeld.quote
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    fill_in 'Password confirmation', with: key
+    click_button 'Create User'
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    click_button 'Log in'
+    click_link ''
   end
 end
