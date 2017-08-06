2a8194217d3d5632e505f9ef97b7efaf30a5413f
Daniel Castro <castrodd@gmail.com>
2017-08-06 18:41:36 -0400

    Fixes for detailed view

--- spec/features/user_creates_a_list_of_cities_spec.rb
+++ spec/features/user_creates_a_list_of_cities_spec.rb
@@ -65,6 +65,23 @@ RSpec.feature "User creates a list of cities", type: :feature do
   end
 
   scenario "User can click on city for more information" do
-
+    mailbox = Faker::Internet.email
+    key = Faker::Seinfeld.quote
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    fill_in 'Password confirmation', with: key
+    click_button 'Create User'
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    click_button 'Log in'
+    click_link 'Add City'
+    fill_in 'Name', with: 'New York'
+    click_button 'Add'
+    click_link 'More Details'
+    expect(page).to have_content 'Currently'
   end
 end
