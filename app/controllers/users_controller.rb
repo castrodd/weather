4dfca6848acfa4f054c548ddaa3dd846f7cdfb45
Daniel Castro <castrodd@gmail.com>
2017-08-06 12:27:04 -0400

    Able to view temp on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @data = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7fec57ebd6b7829c707e3637694695d5')
+    @places = @user.places.all
+    @data = {}
+    @places.each do |city|
+      @data[city] = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{city.name}&APPID=7fec57ebd6b7829c707e3637694695d5")
+    end
   end
 
   private
