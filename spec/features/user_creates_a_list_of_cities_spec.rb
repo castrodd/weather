b3c008b55b6ac425ecf066e7f5f98868f1dac47b
Daniel Castro <castrodd@gmail.com>
2017-08-06 10:23:37 -0400

    Users can delete cities from list

--- spec/features/user_creates_a_list_of_cities_spec.rb
+++ spec/features/user_creates_a_list_of_cities_spec.rb
@@ -1,16 +1,50 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.feature "User creates a list of cities", type: :feature do
   scenario "User can add cities to list of favorites" do
-
+    mailbox = Faker::Internet.email
+    key = Faker::Seinfeld.quote
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    fill_in 'Password confirmation', with: key
+    click_button 'Create User'
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    click_button 'Log in'
+    click_link 'Add City'
+    fill_in 'Name', with: 'New York'
+    click_button 'Add'
+    expect(page).to have_content 'New York'
   end
 
   scenario "User can delete cities from list of favorites" do
-    
+    mailbox = Faker::Internet.email
+    key = Faker::Seinfeld.quote
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    fill_in 'Password confirmation', with: key
+    click_button 'Create User'
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email', with: mailbox
+    fill_in 'Password', with: key
+    click_button 'Log in'
+    click_link 'Add City'
+    fill_in 'Name', with: 'New York'
+    click_button 'Add'
+    click_link 'Delete'
+    expect(page).to have_content 'City removed.'
   end
 
   scenario "User can view current temperature of cities" do
-
+  
   end
 
   scenario "User can click on city for more information" do
