f692cb9373675b27cf1f8d6bafb0aa8ed83a6421
Daniel Castro <castrodd@gmail.com>
2017-08-11 10:35:14 -0400

    Refactoring views

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,40 @@
 module PlacesHelper
+  def set_background(description)
+    if description.include?('thunderstorm')
+      backg = 'thunderstorm.png'
+    elsif description.include?('drizzle')
+      backg = 'drizzle.png'
+    elsif description.include?('rain')
+      backg = 'rain.png'
+    elsif description.include?('snow')
+      backg = 'snow.png'
+    elsif description.include?('clouds')
+      backg = 'clouds.png'
+    elsif description.include?('breeze')
+      backg = 'breeze.png'
+    elsif description.include?('clear')
+      backg = 'clear.png'
+    elsif description.include?('calm')
+      backg = 'calm.png'
+    elsif description.include?('gale')
+      backg = 'gale.png'
+    elsif description.include?('sleet')
+      backg = 'sleet.png'
+    elsif description.include?('mist')
+      backg = 'mist.png'
+    elsif description.include?('smoke')
+      backg = 'smoke.png'
+    elsif description.include?('haze')
+      backg = 'haze.png'
+    elsif description.include?('fog')
+      backg = 'fog.png'
+    elsif description.include?('tropical')
+      backg = 'tropical.png'
+    elsif description.include?('hurricane')
+      backg = 'hurricane.png'
+    else
+      backg = 'default.png'
+    end
+    image_path(backg)
+  end
 end
