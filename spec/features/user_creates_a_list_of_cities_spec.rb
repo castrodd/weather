ac330322e3000f52a58981222b44cc102eb4c8db
Daniel Castro <castrodd@gmail.com>
2017-08-15 20:16:44 -0400

    Updated and refactored tests

--- spec/features/user_creates_a_list_of_cities_spec.rb
+++ spec/features/user_creates_a_list_of_cities_spec.rb
@@ -1,63 +1,36 @@
 require 'rails_helper'
-require 'faker'
 
-RSpec.feature "User creates a list", type: :feature do
-  scenario "User can add cities to list of favorites" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
+RSpec.feature "User creates list", type: :feature do
+
+  before :each do
     visit root_path
     click_link 'Sign up'
-    fill_in 'Email', with: mailbox
-    fill_in 'Password', with: key
-    fill_in 'Password confirmation', with: key
+    fill_in 'Email', with: 'address@email.com'
+    fill_in 'Password', with: 'thisisonlyatest'
+    fill_in 'user_password_confirmation', with: 'thisisonlyatest'
     click_button 'Create Account'
     visit root_path
     click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
+    fill_in 'email', with: 'address@email.com'
+    fill_in 'password', with: 'thisisonlyatest'
     click_button 'Enter'
     click_link 'Add City'
-    fill_in 'Name', with: 'New York'
+  end
+
+  scenario "User can add cities to list of favorites" do
+    fill_in 'place_name', with: 'New York'
     click_button 'Add'
     expect(page).to have_content 'New York'
   end
 
   scenario "User cannot add imaginary cities" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'Email', with: mailbox
-    fill_in 'Password', with: key
-    fill_in 'Password confirmation', with: key
-    click_button 'Create Account'
-    visit root_path
-    click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
-    click_button 'Enter'
-    click_link 'Add City'
-    fill_in 'Name', with: 'Ozymandias'
+    fill_in 'place_name', with: 'Ozymandias'
     click_button 'Add'
     expect(page).to have_content 'You have not added'
   end
 
   scenario "User can delete cities from list of favorites" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'Email', with: mailbox
-    fill_in 'Password', with: key
-    fill_in 'Password confirmation', with: key
-    click_button 'Create Account'
-    visit root_path
-    click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
-    click_button 'Enter'
-    click_link 'Add City'
-    fill_in 'Name', with: 'New York'
+    fill_in 'place_name', with: 'New York'
     click_button 'Add'
     expect(page).to have_content 'New York'
     click_link 'Delete'
@@ -65,39 +38,13 @@ RSpec.feature "User creates a list", type: :feature do
   end
 
   scenario "User can view current temperature of cities" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'Email', with: mailbox
-    fill_in 'Password', with: key
-    fill_in 'Password confirmation', with: key
-    click_button 'Create Account'
-    click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
-    click_button 'Enter'
-    click_link 'Add City'
-    fill_in 'Name', with: 'New York'
+    fill_in 'place_name', with: 'New York'
     click_button 'Add'
     expect(page).to have_content 'F'
   end
 
   scenario "User can click on city for more information" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'Email', with: mailbox
-    fill_in 'Password', with: key
-    fill_in 'Password confirmation', with: key
-    click_button 'Create Account'
-    click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
-    click_button 'Enter'
-    click_link 'Add City'
-    fill_in 'Name', with: 'New York'
+    fill_in 'place_name', with: 'New York'
     click_button 'Add'
     click_link 'More Details'
     expect(page).to have_content 'Currently'
--- spec/features/user_creates_accountand_logs_ins_spec.rb
+++ spec/features/user_creates_accountand_logs_ins_spec.rb
@@ -1,81 +1,54 @@
 require 'rails_helper'
-require 'faker'
 
 RSpec.feature "User creates account", type: :feature do
 
-  scenario "User able to sign up" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
+  before :each do
     visit root_path
     click_link 'Sign up'
-    fill_in 'user_email', with: mailbox
-    fill_in 'user_password', with: key
-    fill_in 'user_password_confirmation', with: key
+    fill_in 'user_email', with: 'mailbox'
+    fill_in 'user_password', with: 'mailboxkey'
+    fill_in 'user_password_confirmation', with: 'mailboxkey'
     click_button 'Create Account'
+  end
+
+  scenario "User able to sign up" do
     expect(page).to have_content "New account created!"
   end
 
   scenario "User able to log in" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'Email', with: mailbox
-    fill_in 'Password', with: key
-    fill_in 'Password confirmation', with: key
-    click_button 'Create Account'
-    visit root_path
     click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
+    fill_in 'email', with: 'mailbox'
+    fill_in 'password', with: 'mailboxkey'
     click_button 'Enter'
     expect(page).to have_content "Welcome"
   end
 
-  scenario "User inputs wrong confirmation details" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'user_email', with: mailbox
-    fill_in 'user_password', with: key
-    fill_in 'user_password_confirmation', with: 'Wrong password'
-    click_button 'Create Account'
-    expect(page).to have_content 'Please try again!'
-  end
-
   scenario "User inputs wrong sign in details" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'user_email', with: mailbox
-    fill_in 'user_password', with: key
-    fill_in 'user_password_confirmation', with: key
-    click_button 'Create Account'
-    visit root_path
     click_link 'Log in'
-    fill_in 'email', with: mailbox
+    fill_in 'email', with: 'mailbox'
     fill_in 'password', with: "Wrong"
     click_button 'Enter'
     expect(page).to have_content "Something was wrong with your email and/or password."
   end
 
   scenario "User signs in and logs out" do
-    mailbox = Faker::Internet.email
-    key = Faker::Seinfeld.quote
-    visit root_path
-    click_link 'Sign up'
-    fill_in 'user_email', with: mailbox
-    fill_in 'user_password', with: key
-    fill_in 'user_password_confirmation', with: key
-    click_button 'Create Account'
-    visit root_path
     click_link 'Log in'
-    fill_in 'email', with: mailbox
-    fill_in 'password', with: key
+    fill_in 'email', with: 'mailbox'
+    fill_in 'password', with: 'mailboxkey'
     click_button 'Enter'
     click_link 'Log out'
     expect(page).to have_content 'Weather Station'
+  end
+end
+
+RSpec.feature "User fails to create account", type: :feature do
+  scenario "User inputs wrong confirmation details" do
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'user_email', with: 'mailbox'
+    fill_in 'user_password', with: 'mailboxkey'
+    fill_in 'user_password_confirmation', with: 'wrongkey'
+    click_button 'Create Account'
+    expect(page).to have_content 'Please try again!'
   end
 end
