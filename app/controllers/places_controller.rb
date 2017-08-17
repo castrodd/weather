e0a398468711f156c70bbe6a24affeeedd76f363
Daniel Castro <castrodd@gmail.com>
2017-08-17 14:51:40 -0400

    Refactored Places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,19 +5,13 @@ class PlacesController < ApplicationController
     @place = current_user.places.new
   end
 
-
   def create
-      valid = place_validate(params[:place][:name])
-      if valid
         @place = current_user.places.new(place_params)
         if @place.save
           redirect_to user_path(current_user), notice: "City added."
         else
-          render :new
+          redirect_to user_path(current_user), notice: "City unavailable. Please try again."
         end
-      else
-        redirect_to user_path(current_user), notice: "City unavailable. Please try again."
-      end
   end
 
   def show
@@ -32,17 +26,9 @@ class PlacesController < ApplicationController
   end
 
   private
+
   def place_params
     place = params.require(:place).permit(:name, :user_id)
   end
 
-  def place_validate(place)
-    #Checks user input and replaces with API's official name
-    data = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{place}&APPID=7fec57ebd6b7829c707e3637694695d5")
-    if data.parsed_response['cod'] == '404'
-      return false
-    else
-      return true
-    end
-  end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,10 @@
 class Place < ApplicationRecord
+  validate :place_validate
+  def place_validate
+    data = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{name}&APPID=7fec57ebd6b7829c707e3637694695d5").parsed_response
+    if data['cod'] != 200 || data['name'].downcase != name.downcase
+      errors.add(:name, "This city does not exist!")
+    end
+  end
   belongs_to :user
 end
